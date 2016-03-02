d36762fb0150552f5ea64698376c42cff6d10cc2
thtruo <ttruongchau@gmail.com>
2016-03-02 01:55:07 -0800

    Initial Commit

---
+++ VocalizerTests/VocalizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  VocalizerTests.swift
+//  VocalizerTests
+//
+//  Created by Thomas Truongchau on 3/2/16.
+//  Copyright © 2016 Thomas Truongchau. All rights reserved.
+//
+
+import XCTest
+@testable import Vocalizer
+
+class VocalizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
