aa233a175942cc32b4b10667210a27b2f9a694c5
thtruo <ttruongchau@gmail.com>
2016-03-12 16:54:16 -0800

    Implemented a chipmunk-like pitch sound effect. Users can tap a button to play their recording in a high pitch.

--- Vocalizer/PlaySoundsViewController.swift
+++ Vocalizer/PlaySoundsViewController.swift
@@ -12,17 +12,16 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
 
     var audioPlayer: AVAudioPlayer!
+    var audioEngine: AVAudioEngine!
     var receivedAudio: RecordedAudio!
+    var audioFile: AVAudioFile!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
         audioPlayer.enableRate = true
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
     }
 
     func playAudioHelper(rate: Float) {
@@ -35,9 +34,39 @@ class PlaySoundsViewController: UIViewController {
     @IBAction func playSlowAudio(sender: UIButton) {
         playAudioHelper(0.7)
     }
+
     @IBAction func playFastAudio(sender: UIButton) {
         playAudioHelper(2.0)
     }
+
+    func playAudioWithVariablePitch(pitch: Float) {
+        // Stop the audio player and reset the audio engine
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+
+        // Set up the audioPlayerNode and changePitchEffect
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+
+        // Connect the audioPlayerNode to the new pitch effect to the speaker
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+
+        // Play the audio with new sound effects
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        audioPlayerNode.play()
+    }
+
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+
     @IBAction func stopRecordingAudio(sender: UIButton) {
         audioPlayer.stop()
     }
